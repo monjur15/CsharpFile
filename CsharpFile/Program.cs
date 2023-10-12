28655a00c1bbe38fa85dbe5f8fef1d88d3c8d2e3
Monjur <monjur.morshed@azolutionse.com>
2023-10-12 13:28:37 +0600

    first commit

---
+++ CsharpFile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CsharpFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"D:\Temp\Demos\FileSystem";
+            string[] dirs = Directory.GetDirectories(rootPath);
+            
+
+            foreach (var dir in dirs)
+            {
+                Console.WriteLine(dir);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
